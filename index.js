50220b1607fd75392d9eb823c22b42ca3d03df82
Jordan Yee <jordanyee799@gmail.com>
2021-09-22 17:02:49 -0700

    Compmleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function appendCat(name) {
+    var newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    var newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(name) {
+    var newCats = [...cats];
+    newCats.pop()
+    return newCats;
+}
+
+function removeFirstCat() {
+    var newCats = [...cats];
+    newCats.shift()
+    return newCats;
+}
